67a5ea94c21ea6590cd789a29e350751b6388dae
guying18 <guying828@foxmail.com>
2021-12-21 14:43:14 +0800

    feat: add ErrorBoundary

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -63,6 +63,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   if (isError) {
     return <FullPageErrorFallback error={error} />;
   }
+
   return (
     <AuthContext.Provider
       children={children}
